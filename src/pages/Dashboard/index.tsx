591f0bcdf8493ae82808c78236c511c02352056a
jerp86 <jerp4@hotmail.com>
2020-04-20 22:29:49 -0300

    Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,86 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/54115624?s=460&u=6dd0a7b2e45858cdb7b732f16721d4bca13cfe66&v=4"
+            alt="Jerp86"
+          />
+          <div>
+            <strong>team-organizer-backend</strong>
+            <p>
+              Team organizer for hackathons by roles and experiences.
+              https://teammaker.netlify.com
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/54115624?s=460&u=6dd0a7b2e45858cdb7b732f16721d4bca13cfe66&v=4"
+            alt="Jerp86"
+          />
+          <div>
+            <strong>team-organizer-backend</strong>
+            <p>
+              Team organizer for hackathons by roles and experiences.
+              https://teammaker.netlify.com
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/54115624?s=460&u=6dd0a7b2e45858cdb7b732f16721d4bca13cfe66&v=4"
+            alt="Jerp86"
+          />
+          <div>
+            <strong>team-organizer-backend</strong>
+            <p>
+              Team organizer for hackathons by roles and experiences.
+              https://teammaker.netlify.com
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/54115624?s=460&u=6dd0a7b2e45858cdb7b732f16721d4bca13cfe66&v=4"
+            alt="Jerp86"
+          />
+          <div>
+            <strong>team-organizer-backend</strong>
+            <p>
+              Team organizer for hackathons by roles and experiences.
+              https://teammaker.netlify.com
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
